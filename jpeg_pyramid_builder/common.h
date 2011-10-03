8d60004bac195c533c75b75d3f535611a36cd2dd
Gerben van Veenendaal <gerbenvv@gmail.com>
2011-10-03 11:39:17 +0000

    Added common header.
    git-svn-id: https://svn.science.uu.nl/repos/edu.3470784.Softwareproject_C@13 54bd1c4f-d025-4ee3-8271-c04213acb236

---
+++ jpeg_pyramid_builder/common.h
@@ -0,0 +1,24 @@
+/*
+ * Common file.
+ *
+ */
+
+#ifndef _COMMON_H_
+#define	_COMMON_H_
+
+//include all needed headers
+#include <cstdio>
+#include <stdexcept>
+#include <exception>
+#include <cmath>
+#include <sstream>
+#include <cassert>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <jpeglib.h>
+
+//use standard library
+using namespace std;
+
+#endif /* _COMMON_H_ */
