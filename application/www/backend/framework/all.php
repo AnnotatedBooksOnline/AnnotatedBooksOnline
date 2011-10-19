a7ad40370ed9f562a28abcd681b5fa8677167c5e
Gerben van Veenendaal <gerbenvv@gmail.com>
2011-10-19 19:43:46 +0200

    Added basic framework directory structure, changed overall directory structure.

---
+++ application/www/backend/framework/all.php
@@ -0,0 +1,8 @@
+<?php
+
+//require all framework components
+require 'database.php';
+require 'session.php';
+require 'cache.php';
+require 'controller.php';
+require 'page.php';
---
+++ application/www/backend/index.php
@@ -0,0 +1,9 @@
+<?php
+
+//require framework
+require 'framework/all.php';
+
+//require authentication
+require 'util/authentication.php';
+
+//NOTE: let controller handle the request
