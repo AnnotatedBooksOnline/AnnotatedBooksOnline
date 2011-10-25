8ef9924fae1795c938189caf72d651bff3b69c96
mathijsje <mathijsbaaijens@mathijsje-L.local>
2011-10-26 01:08:16 +0200

    Copyright header toegevoegd

--- application/www/backend/framework/database/database.php
+++ application/www/backend/framework/database/database.php
@@ -1,4 +1,19 @@
 ﻿<?php 
+/*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; version 3 of the License.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* Copyright: Bert Massop, Gerben van Veenendaal, Iris Bekker, Jeroen Hanselman,
+* Maarten van Duren, Renze Droog, Robin van der Ploeg, Tom Tervoort,
+* Tom Wennink, Mathijs Baaijens.
+*/
+
 
 define("DB_TEST_DSN", "pgsql:dbname=test;host=localhost");
 define("DB_TEST_USERNAME", "postgres");
