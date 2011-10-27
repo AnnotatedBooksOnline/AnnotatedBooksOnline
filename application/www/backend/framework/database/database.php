471adbfcb71276bc325a92cb31f687d8a17fe83b
tomtervoort <tomtervoort@gmail.com>
2011-10-27 14:19:54 +0200

    Split Low-level database API in two files.
    ResultSet and related classes can now be found in resultset.php.
    
    COLLAB-9

--- application/www/backend/framework/database/database.php
+++ application/www/backend/framework/database/database.php
@@ -1,6 +1,7 @@
 ﻿<?php
 
 require_once 'framework/helpers/singleton.php';
+require_once 'framework/database/resultset.php';
 
 // TODO: Read these from ini-file
 define("DB_TEST_DSN", "pgsql:dbname=test;host=localhost");
@@ -18,144 +19,9 @@ class DBException extends Exception
 }
 
 /**
- * An IteratorAggregate for the results of a query.
+ * A singleton class representing a connection to the database. With this database queries can be executed. 
  *
- * Can be used to iterate over the rows in the result set of a query.
- *
- * Example:
- * \verbatim
- * $results = $dbc->query('SELECT user_name FROM users WHERE birth_date = %a', unixtojd());
- * foreach($results as $result)
- * {
- *      print 'Happy birthday, ' . $result->getValue('user_name') . '!\n'
- * }
- * \endverbatim
  */
-class ResultSet implements IteratorAggregate
-{
-    public $statement;
-
-    // TODO: why static, that way we can't have two resultsets at the same time, or iterate them.
-    // TODO: this is quite a common usecase however: foreach (ResultSet a) { foreach (ResultSet b) { .. } }
-
-    public function __construct($pdo_stat)
-    {
-        $this->statement = $pdo_stat;
-    }
-
-    public function __destruct()
-    {
-        $this->statement->closeCursor();
-    }
-
-    public function getIterator()
-    {
-        return new ResultSetIterator($this, $this->statement);
-    }
-}
-
-/**
- * An iterator of ResultSetRows used by ResultSet.
- */
-class ResultSetIterator implements Iterator
-{
-    private $rset;
-    private $statement;
-    private $i;
-    private $curr;
-
-    public function __construct($rset, $stat)
-    {
-        $this->rset      = $rset;
-        $this->statement = $stat;
-        $this->i         = -1;
-        $this->next();
-    }
-
-    public function next()
-    {
-        ++$this->i;
-        if ($this->i > $this->statement->rowCount())
-        {
-            throw new Exception("Iterator out of range.");
-        }
-
-        $this->curr = $this->statement->fetch(PDO::FETCH_ASSOC);
-    }
-
-    public function key()
-    {
-        return $this->i;
-    }
-
-    public function current()
-    {
-        return new ResultSetRow($this->curr);
-    }
-
-    public function valid()
-    {
-        return $this->i < $this->statement->rowCount();
-    }
-
-    public function rewind()
-    {
-        if ($this->i > 0)
-        {
-            throw new Exception("Cannot rewind a ResultSetIterator.");
-        }
-    }
-}
-
-/**
- * Contains the items in a result set from a single row.
- */
-class ResultSetRow
-{
-    private $row;
-
-    public function __construct($row)
-    {
-        foreach ($row as $key => $val)
-        {
-            $this->row[strtoupper($key)] = $val;
-        }
-    }
-
-    /**
-     * Get the value from the column with the specified name as a string.
-     *
-     * @param string $columnname The name of the column, is case incensitive.
-     *
-     * @return A string representing the value stored at the specified column, or NULL if there is
-     *            no column with this name.
-     */
-    public function getValue($columnname)
-    {
-        if (array_key_exists(strtoupper($columnname), $this->row))
-        {
-            return $this->row[strtoupper($columnname)];
-        }
-        else
-        {
-            return NULL;
-        }
-    }
-
-    // TODO: get value as number, date, blob etc.
-
-    /**
-     * Returns an associative array with the column names and corresponding alues.
-     *
-     * Note that the column names are converted to upper case.
-     *
-     */
-    public function getValues()
-    {
-        return $this->row;
-    }
-}
-
 class DBConnection extends Singleton
 {
     private $pdo;
@@ -494,9 +360,9 @@ class DBConnection extends Singleton
 }
 
 // Test
-/* $dbc = DBConnection::getInstance();
+$dbc = DBConnection::getInstance();
 $result = $dbc->query('select * from testtabel where testid >= %d',6);
 foreach($result as $row)
 {
     echo $row->getValue('blah') . "</br>";
-} */
+}
---
+++ application/www/backend/framework/database/resultset.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+* An IteratorAggregate for the results of a query.
+*
+* Can be used to iterate over the rows in the result set of a query.
+*
+* Example:
+* \verbatim
+* $results = $dbc->query('SELECT user_name FROM users WHERE birth_date = %a', unixtojd());
+* foreach($results as $result)
+* {
+*      print 'Happy birthday, ' . $result->getValue('user_name') . '!\n'
+* }
+* \endverbatim
+*/
+class ResultSet implements IteratorAggregate
+{
+    public $statement;
+
+    public function __construct($pdo_stat)
+    {
+        $this->statement = $pdo_stat;
+    }
+
+    public function __destruct()
+    {
+        $this->statement->closeCursor();
+    }
+
+    public function getIterator()
+    {
+        return new ResultSetIterator($this, $this->statement);
+    }
+}
+
+/**
+ * An iterator of ResultSetRows used by ResultSet.
+ */
+class ResultSetIterator implements Iterator
+{
+    private $rset;
+    private $statement;
+    private $i;
+    private $curr;
+
+    public function __construct($rset, $stat)
+    {
+        $this->rset      = $rset;
+        $this->statement = $stat;
+        $this->i         = -1;
+        $this->next();
+    }
+
+    public function next()
+    {
+        ++$this->i;
+        if ($this->i > $this->statement->rowCount())
+        {
+            throw new Exception("Iterator out of range.");
+        }
+
+        $this->curr = $this->statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function key()
+    {
+        return $this->i;
+    }
+
+    public function current()
+    {
+        return new ResultSetRow($this->curr);
+    }
+
+    public function valid()
+    {
+        return $this->i < $this->statement->rowCount();
+    }
+
+    public function rewind()
+    {
+        if ($this->i > 0)
+        {
+            throw new Exception("Cannot rewind a ResultSetIterator.");
+        }
+    }
+}
+
+/**
+ * Contains the items in a result set from a single row.
+ */
+class ResultSetRow
+{
+    private $row;
+
+    public function __construct($row)
+    {
+        foreach ($row as $key => $val)
+        {
+            $this->row[strtoupper($key)] = $val;
+        }
+    }
+
+    /**
+     * Get the value from the column with the specified name as a string.
+     *
+     * @param string $columnname The name of the column, is case incensitive.
+     *
+     * @return A string representing the value stored at the specified column, or NULL if there is
+     *            no column with this name.
+     */
+    public function getValue($columnname)
+    {
+        if (array_key_exists(strtoupper($columnname), $this->row))
+        {
+            return $this->row[strtoupper($columnname)];
+        }
+        else
+        {
+            return NULL;
+        }
+    }
+
+    // TODO: get value as number, date, blob etc.
+
+    /**
+     * Returns an associative array with the column names and corresponding alues.
+     *
+     * Note that the column names are converted to upper case.
+     *
+     */
+    public function getValues()
+    {
+        return $this->row;
+    }
+}
