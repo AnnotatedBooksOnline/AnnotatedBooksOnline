326fb7dc868e1fd62f186eb620f411c25cb19398
tomtervoort <tomtervoort@gmail.com>
2011-11-11 13:05:22 +0100

    Made first DB update script, allowing multiple languages for books.
    Also added two primary key constraints I apperantly forgot. I made both an
    update script and a new total creation script, although the latter isn't
    really necessary.
    
    Updating is not yet necessary for the first increment, I think. Unless it's
    already desired for the search functionality.

---
+++ application/sqlscripts/updates/update0_1.sql
@@ -0,0 +1,11 @@
+﻿
+-- Add two forgotten primary key constraints.
+ALTER TABLE "Authors" ADD PRIMARY KEY ("authorId", "bookId");
+ALTER TABLE "PendingUsers" ADD PRIMARY KEY ("userId");
+
+-- Allow books to have multiple languages.
+ALTER TABLE "Books" DROP COLUMN "language";
+ALTER TABLE "BindingLanguages" DROP CONSTRAINT "BindingLanguages_pkey";
+ALTER TABLE "BindingLanguages" ADD COLUMN "bookID" serial NOT NULL;
+ALTER TABLE "BindingLanguages" RENAME TO "BookLanguages";
+ALTER TABLE "BookLanguages" ADD PRIMARY KEY ("bindingId", "bookID", "language");
