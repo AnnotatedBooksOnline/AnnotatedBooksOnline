a83c2c38fff580a2470a50d1fe2265f7580b801d
Gerben van Veenendaal <gerbenvv@gmail.com>
2011-12-07 11:53:51 +0100

    Added polygons overlay to the viewer, implemented most of the dragging of vertices and implemented creating new ones.
    COLLAB-188
    COLLAB-64

---
+++ application/www/frontend/viewer/overlay.js
@@ -0,0 +1,73 @@
+/*
+ * Use strict mode if available.
+ */
+
+"use strict";
+
+/*
+ * Overlay class.
+ */
+
+// Class definition.
+function Overlay()
+{
+    if (arguments.length)
+        this.constructor.apply(this, arguments);
+}
+
+Overlay.prototype = new DomNode;
+Overlay.base = DomNode.prototype;
+
+// Fields.
+Overlay.prototype.position;
+Overlay.prototype.rotation;
+Overlay.prototype.zoomLevel;
+Overlay.prototype.zoomFactor;
+Overlay.prototype.invZoomFactor;
+
+// Constructor.
+Overlay.prototype.constructor = function()
+{
+    // Create dom.
+    Overlay.base.constructor.call(this, '<div class="overlay"></div>');
+    
+    // Initialize.
+    this.initialize();
+}
+
+/*
+ * Public methods.
+ */
+
+Overlay.prototype.update = function(position, zoomLevel, rotation, area)
+{
+    this.position      = position;
+    this.rotation      = rotation;
+    this.zoomLevel     = zoomLevel;
+    this.zoomFactor    = Math.pow(2, zoomLevel);
+    this.invZoomFactor = 1 / this.zoomFactor;
+}
+
+Overlay.prototype.transformPoint = function(point)
+{
+    // Get offset of viewport dom.
+    var domOffset = this.dom.offset();
+    
+    // Calculate point within overlay in viewport dimensions.
+    var point = {
+        x: (point.x - domOffset.left) * this.invZoomFactor + this.position.x,
+        y: (point.y - domOffset.top)  * this.invZoomFactor + this.position.y
+    };
+    
+    // Rotate point back to document coordinates.
+    return rotatePoint(point, -this.rotation);
+}
+
+/*
+ * Private methods.
+ */
+
+Overlay.prototype.initialize = function()
+{
+    // Implement in subclass.
+}
