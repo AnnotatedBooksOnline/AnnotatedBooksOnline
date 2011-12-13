105c061e7f5c59a7c48d7064bf1a9851d2cf5976
Mathijs Baaijens <mathijsbaaijens@mathijsje-L.local>
2011-12-13 04:06:28 +0100

    COLLAB-138 Created entity classes required to implement upload.

--- application/www/backend/models/library/library.php
+++ application/www/backend/models/library/library.php
@@ -9,4 +9,95 @@ require_once 'framework/database/entity.php';
 class Library extends Entity
 {
     
+    /** Id of this library. */
+    protected $libraryId;
+    
+    /** Library name. */
+    protected $libraryName;
+    
+    /** Library address */
+    protected $libraryAddress;
+    
+    /** Library website. */
+    protected $website;
+    
+    /** Info text about this library. */
+    protected $info;
+       
+    /**
+     * Constructs a library by id.
+     *
+     * @param  $id  Id of the library. Default (null) will create a new library.
+     */
+    public function __construct($libraryId = null)
+    {
+        if ($id !== null)
+        {
+            $this->libraryId = $libraryId;
+            $this->load();
+        }
+    }
+    
+    /**
+     * Gets the table name.
+     *
+     * @return  The table name.
+     */
+    protected function getTableName()
+    {
+        return 'Libraries';
+    }
+    
+    /**
+     * Gets the primary keys.
+     *
+     * @return  Array of all primary keys.
+     */
+    protected function getPrimaryKeys()
+    {
+        return array('libraryId');
+    }
+    
+    /**
+     * Gets all the columns.
+     *
+     * @return  Array of all columns, except primary keys.
+     */
+    protected function getColumns()
+    {
+        return array('libraryId', 'libraryName', 'libraryAddress', 'website', 'info');
+    }
+    
+    /**
+     * Gets all the column types, per column, including primary keys.
+     *
+     * @return  Array of all column types.
+     */
+    protected function getColumnTypes()
+    {
+        return array(
+                        'libraryId'        => 'int',
+                        'libraryName'      => 'string',
+                        'libraryAddress'   => 'string',
+                        'website'          => 'string',
+                        'info'             => 'string',
+        );
+    }
+    
+    public function getLibraryId()               { return $this->libraryId; }
+    public function setLibraryId($libraryId)     { $this->libraryId = $libraryId; }
+    
+    public function getLibraryName()             { return $this->libraryName; }
+    public function setLibraryName($libraryName) { $this->libraryName = $libraryName; }
+    
+    public function getLibraryAddress()                 { return $this->libraryAddress; }
+    public function setLibraryAddress($libraryAddress)  {  $this->libraryAddress = libraryAddress; }
+    
+    public function getWebsite()         { return $this->website; }
+    public function setWebsite($website) { $this->website = $website; }
+    
+    public function getInfo()          { return $this->info; }
+    public function setInfo($info)     { $this->info = $info; }
+    
+    
 }
