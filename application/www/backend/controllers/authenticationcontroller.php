AnnotatedBooksOnline/AnnotatedBooksOnline

commit 42b054f6d2bff6badfaa2170227591892587b4ae

Check for current password in editprofile
Not tested.
COLLAB-152

application/www/backend/controllers/authenticationcontroller.php
@@ -26,6 +26,14 @@ class AuthenticationController extends Controller
         // Logout.
         Authentication::getInstance()->logout();
     }
+{
+    public function actionCheckPassword($data)
+    {
+        // Get field.
+        $password = self::getString($data, 'password', '', false, 32);
+        
+        return Authentication::getInstance()->checkPassword($password);
+    }
     
     public function actionKeepAlive($data)
     {

application/www/backend/util/authentication.php
@@ -132,6 +132,26 @@ class Authentication extends Singleton
     }
     
     /**
+     * Checks a password of an user.
+     *
+     * @param  $password  The password of the user to check.
+     *
+     * @return true or false.
+     */
+    public function checkPassword($password)
+    {
+        $currentUser = $this->getUser();
+        if ($currentUser != null && User::checkPassword($this->getUserId(), $password)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    /**
      * Checks whether a user is logged on.
      *
      * @return  Whether a user is logged on.

application/www/backend/models/user/user.php
@@ -121,6 +121,35 @@ class User extends Entity
     }
     
     /**
+     * Checks the password of the userId.
+     *
+     * @param  $userId  UserId of the user.
+     * @param  $password  Password of the user.
+     *
+     * @return  Correctness of password.
+     */
+    public static function checkPassword($userId, $password)
+    {
+        $query = Query::select('userId')->
+                 from('Users')->
+                 where('userId = :userId', 'passwordHash = :hash');
+        
+        $resultSet = $query->execute(array(
+            'userId' => $userId,
+            'hash'   => self::secureHash($password)
+        ));
+        
+        if ($resultSet->getAmount() == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    /**
      * Gets the table name.
      *
      * @return  The table name.

application/www/frontend/logic/authentication.js
@@ -280,6 +280,21 @@ Authentication.prototype.login = function(username, password, obj, onSuccess, on
         }, onError);
 }
 
+Authentication.prototype.checkPassword = function(password, obj, onSuccess, onError)
+{
+    // Do a check password request.
+    RequestManager.getInstance().request('Authentication', 'checkPassword', {password: password},
+        this,
+        function(data)
+        {
+            // Call success handler
+            if (onSuccess !== undefined)
+            {
+                onSuccess.call(obj);
+            }
+        }, onError);
+}
+
 /*
  * Private methods.
  */

application/www/frontend/views/editprofilewindow.js
@@ -181,18 +181,32 @@ Ext.define('Ext.ux.EditProfileForm', {
         var form = this.getForm();
         if (form.isValid())
         {
-            this.saveModel(this,
+            Authentication.getInstance().checkPassword(form.down('[name=password]'), this,
                 function()
                 {
-                    // Fire model changed event.
-                    Authentication.getInstance().modelChanged();
-                    
-                    // Close window above us, if any.
-                    var window = this.up('window');
-                    if (window)
-                    {
-                        window.close();
-                    }
+                    this.saveModel(this,
+                        function()
+                        {
+                            // Fire model changed event.
+                            Authentication.getInstance().modelChanged();
+                            
+                            // Close window above us, if any.
+                            var window = this.up('window');
+                            if (window)
+                            {
+                                window.close();
+                            }
+                        });
+                },
+                function(code, message, trace)
+                {
+                    // Show an error.
+                    Ext.Msg.show({
+                        title: 'Wrong current password.',
+                        msg: message, //'Combination of username and password could not be found.',
+                        icon: Ext.Msg.ERROR,
+                        buttons: Ext.Msg.OK
+                    });
                 });
         }
     }

commit 3fd7d4c65ee04893ae2b12c98172480959a988a9

Fix errors introduced by Renze in commit 42b054f6d2bff6badfaa2170227591892587b4ae: Check for current password in editprofile
Not tested on functionality.

COLLAB-152

application/www/backend/controllers/authenticationcontroller.php
@@ -26,7 +26,7 @@ class AuthenticationController extends Controller
         // Logout.
         Authentication::getInstance()->logout();
     }
-{
+
     public function actionCheckPassword($data)
     {
         // Get field.

application/www/backend/util/authentication.php
@@ -132,7 +132,7 @@ class Authentication extends Singleton
     public function checkPassword($password)
     {
         $currentUser = $this->getUser();
-        if ($currentUser != null && User::checkPassword($this->getUserId(), $password)
+        if ($currentUser != null && User::checkPassword($this->getUserId(), $password))
         {
             return true;
         }