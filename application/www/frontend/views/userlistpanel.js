4ff7f74ac7a916d09ae5eb15dfafb2580091f689
Renze Droog <renzedroog@gmail.com>
2011-12-21 17:49:02 +0100

    Editprofile without current password check, userlist links
    Editprofile doesn't use current password anymore, will be included again later. Links in the userlist work fine now.
    
    COLLAB-226
    COLLAB-263

--- application/www/frontend/views/userlistpanel.js
+++ application/www/frontend/views/userlistpanel.js
@@ -90,7 +90,7 @@ Ext.define('Ext.ux.UserListPanel', {
                     itemclick: function(view, record)
                     {
                         // Open user in a new tab if not clicked on 'email' or 'website'.
-                        if (record.get('dataIndex')!='email' && record.get('dataIndex')!='website')
+                        if (view.get('dataIndex')!='email' && view.get('dataIndex')!='website')
                         {
                             var name = record.get('username');
                             Application.getInstance().gotoTab('viewprofile', [name], true);
