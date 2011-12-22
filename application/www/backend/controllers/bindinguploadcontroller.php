49ca0ac6a53f4340d59c7714d7043c1b1922d054
Mathijs Baaijens <mathijsbaaijens@mathijsje-L.local>
2011-12-22 23:31:23 +0100

    COLLAB-138 Fixed the upload handler

--- application/www/backend/controllers/bindinguploadcontroller.php
+++ application/www/backend/controllers/bindinguploadcontroller.php
@@ -6,7 +6,8 @@ require_once 'util/authentication.php';
 require_once 'models/upload/upload.php';
 require_once 'models/book/book.php';
 require_once 'models/book/booklist.php';
-require_once 'models/book/binding.php';
+require_once 'models/binding/binding.php';
+require_once 'models/library/librarysearchlist.php';
 
 /**
  * Binding upload controller class.
@@ -18,102 +19,79 @@ class BindingUploadController extends Controller
      */
     public function actionUpload($data)
     {
+        //var_dump($data);
         // TODO: exceptions
         
         // Assert that the user is authenticated. 
-        Authentication::assertLoggedOn();
-        
-        // Create binding.
-        $binding = new Binding();        
+        Authentication::assertLoggedOn();     
         
         // Retrieve contents of record.
-        $record = self::getArray($data, 'record');
-        $scans = $record['scans'];
-        $binding = $record['binding'];
-        $books = $record['books'];
+        $inputScans = self::getArray($data, 'scans');
+        $inputBinding = self::getArray($data, 'binding');
+        $inputBooks = self::getArray($data,'books');
         
         // Transaction.
-        Database::getInstance()->doTransaction(
-        function() use ($scans, $binding, $books)
-        {
-            // Determine the binding's library.
-            $library = $binding['library'];
-            $lentity = new Library();
-            $lentity->setLibraryName($library);
+        //Database::getInstance()->doTransaction(
+        //function() use ($inputScans, $inputBinding, $inputBooks)
+        //{
+            // Find the name of the library the binding belongs to.
+            $libraryName = $inputBinding['library'];
+        
+            // Create the binding and fill its attributes with the information from the request.
+            $binding = new Binding();
+            $binding->setSignature(self::getString($inputBinding, 'signature'));
+            $binding->setSummary(self::getString($inputBinding, 'summary'));
             
-            $lresult = $lentity->getSelectQuery()->where('libraryName = :libname')
-                                                 ->execute(array('libname' => $library));
-                                                 
+            // Find the specified library in the database.
+            $existingLibrary = LibrarySearchList::findLibraries(array('libraryName' => $libraryName), null, null, null)->getFirstRow_();
             
-            if($lresult->getRowCount() == 0)
+            // Determine if the library exists in the database. If this is the case the new binding should link to it. If not
+            // the library needs to be created.
+            if ($existingLibrary) 
             {
-                // Library does not yet exist, create one.    
-                $lentity->save();
+                // Make the new binding link to the existing library.
+                $binding->setLibraryId($existingLibrary->getValue('libraryId'));    
+            } 
+            else 
+            {
+                // Create a new library and save it.
+                $library = new Library();
+                $library->setLibraryName($libraryName);
+                $library->save();
+                
+                // Link the binding to the newly created library.
+                $binding->setLibraryId($library->getLibraryId());
             }
-            else
-            {
-                // Library does exist, load it.
-                $lentity->load();
-            }
-                             
-            // Create binding.
-            $binentity = new Binding();
-            $binentity->setLibraryId($lentity->getLibraryId());
-            $binentity->setSignature($binding['signature']);
-            if(isset($binding['summary']))
-                $binentity->setSummary($binding['summary']);
-            $binentity->save();
             
-            //TODO: provenance
-            
-            // Create books.
-            foreach($books as $book)
+            // Iterate over all books in the input.
+            foreach($inputBooks as $inputBook)
             {
-                $bentity = new Book();
-                $bentity->setTitle($book['title']);
-                $bentity->setBindingId($binentity->getBindingId());
-                $bentity->setMinYear($book['minYear']);
-                $bentity->setMaxYear($book['maxYear']);
-                if(isset($book['preciseDate']))
-                {
-                    $bentity->setPreciseDate($book['preciseDate']);
-                }
-                if(isset($book['placePublished']))
-                {
-                    $bentity->setPlacePublished($book['placePublished']);
-                }
-                if(isset($book['publisher']))
-                {
-                    $bentity->setPublisher($book['publisher']);
-                }
-                if(isset($book['printVersion']))
-                {
-                    $bentity->setPrintVersion($book['printVersion']);
-                }
+                // Create the book and fill its attributes with the information from the request.
+                $book = new Book();
+                $book->setTitle(self::getString($inputBook, 'title'));
+                $book->setMinYear(self::getInteger($inputBook, 'minYear'));
+                $book->setMaxYear(self::getInteger($inputBook, 'maxYear'));
+                $book->setPreciseDate(self::getString($inputBook, 'preciseDate'));
+                $book->setPlacePublished(self::getString($inputBook, 'placePublished'));
+                $book->setPublisher(self::getString($inputBook, 'publisher'));
+                $book->setPrintVersion(self::getInteger($inputBook, 'printVersion'));
+                
+                // Add the book to the binding.
+                $binding->getBookList()->addEntity($book);
                 
                 //TODO: author
             }
             
             // Create scans.
-            foreach($scans as $scan)
+            foreach($inputScans as $scan)
             {
-                $sentity = new Scan();
+                //$sentity = new Scan();
                 //TODO: scans (token, filename, 
             }
-        }); 
-        
-        // Create the books.
-        for ($i = 0; $i < 10; $i++)
-        {
-            // Create book.
-            $book = new Book();
             
-            // TODO: Fill the book details.
-            
-            $binding->getBookList()->addEntity($book);
-        }
-        
-        // Save everything.
-        $binding->save();
+            // Save the binding and all its attribute entities.
+            $binding->saveWithDetails();
+       //}); 
+      
     }
 }
