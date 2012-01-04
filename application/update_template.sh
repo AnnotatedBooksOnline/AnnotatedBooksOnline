95cda6f40c57fa350fc25b08c4604120fab53e94
Mathijs Baaijens <mathijsbaaijens@mathijsje-L.local>
2012-01-04 12:32:45 +0100

    Made the update template work for other environment than just deftest.

--- application/update_template.sh
+++ application/update_template.sh
@@ -38,7 +38,7 @@ mkdir bin
 cp -f application/tilepyramidbuilder/pyramid_builder bin/pyramid_builder
 
 echo "Deploying cronjob..."
-old_php_instance=$(ps aux | grep "php cronjob/pyramidbuilder" | grep "devtest" | grep -v "grep" | awk '{print $2}')
+old_php_instance=$(ps aux | grep "php cronjob/pyramidbuilder" | grep $ENVIRONMENT | grep -v "grep" | awk '{print $2}')
 
 # TODO : TBH I dont think this does what i want it to do :D
 if [ -n $old_php_instance ];
@@ -63,7 +63,7 @@ cd application/application/sqlscripts/updates
 lastupdate=$(echo update*_*.sql | tr " " "\n" | sed 's/update\([0-9]\+\)_.*/\1/' | sort -n | tail -n 1)
 for i in $(seq 0 $lastupdate)
 do
-    cat update${i}_$(($i+1)).sql | psql devtest postgres 2>/dev/null >/dev/null
+    cat update${i}_$(($i+1)).sql | psql $ENVIRONMENT postgres 2>/dev/null >/dev/null
 done
 cd ../../../..
 
