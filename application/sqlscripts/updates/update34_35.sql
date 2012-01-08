5cd2bd4f9d8ccdc5545d5870f4bc7d1b8e176842
Renze Droog <renzedroog@gmail.com>
2012-01-08 04:02:08 +0100

    Added permission settings

---
+++ application/sqlscripts/updates/update34_35.sql
@@ -0,0 +1,11 @@
+START TRANSACTION;
+
+-- Inserts new actions in Permissions
+--   http://sp.urandom.nl:8091/display/WCOLLAB/Permissions+table
+
+DELETE FROM "Permissions" WHERE "actionName" = 'view-users';
+
+INSERT INTO "Permissions" ("actionName", "minRank") VALUES ('view-users-part', 10);
+INSERT INTO "Permissions" ("actionName", "minRank") VALUES ('view-users-complete', 50);
+
+COMMIT;
