11ad4e4c0e27b6a85956a268cfffb8433e8572c2
tomtervoort <tomtervoort@gmail.com>
2012-01-12 14:28:13 +0100

    Added HelpPage and HelpControlItem entities.
    COLLAB-352

---
+++ application/www/backend/models/help/helpcontrolitem.php
@@ -0,0 +1,73 @@
+<?php
+//[[GPL]]
+
+require_once 'framework/database/entity.php';
+require_once 'models/help/helppage.php';
+
+/**
+ * Entity representing a so-called control item of a help page.
+ */
+class HelpControlItem extends Entity
+{
+    /** Name and primary key of the control item. */
+    protected $controlItemName;
+    
+    /** Page to which this item belongs. */
+    protected $helpPageId;
+    
+    /** A marker in the page being referred that should be jumped to when clicking on this item. */
+    protected $marker;
+    
+    /**
+    * Constructs a HelpControlItem by name.
+    *
+    * @param string $name Name of the control item. Default (null) will create a new one.
+    */
+    public function __construct($name = null)
+    {
+        if($name !== null)
+        {
+            $this->controlItemName = $name;
+            $this->load();
+        }
+    }
+    
+    
+    // Standard entity functions.
+    
+    protected function getTableName()
+    {
+        return 'HelpControlItems';
+    }
+    
+    protected function getPrimaryKeys()
+    {
+        return array('controlItemName');
+    }
+    
+    protected function getColumns()
+    {
+        return array('helpPageId', 'marker');
+    }
+    
+    protected function getColumnTypes()
+    {
+        return array(
+            'controlItemName' => 'string',
+            'helpPageId'      => 'int',
+            'marker'          => 'string'
+        );
+    }
+    
+    
+    // Getters and setters.
+    
+    public function getControlItemName()      { return $this->controlItemName;  }
+    public function setControlItemName($name) { $this->controlItemName = $name; }
+    
+    public function getHelpPageId()    { return $this->helpPageId; }
+    public function setHelpPageId($id) { $this->helpPageId = $id;  }
+    
+    public function getMarker()        { return $this->marker;    }
+    public function setMarker($marker) { $this->marker = $marker; }
+}
---
+++ application/www/backend/models/help/helppage.php
@@ -0,0 +1,133 @@
+<?php
+//[[GPL]]
+
+require_once 'framework/database/entity.php';
+require_once 'models/help/helpcontrolitem.php';
+
+/**
+ * Entity representing a help page.
+ */
+class HelpPage extends Entity
+{    
+    /** Identifier. */
+    protected $helpPageId;
+    
+    /** The name of the page. */
+    protected $pageName;
+    
+    /** The content of the page, in HTML. */
+    protected $content;
+    
+    /** The parent page, if any. */
+    protected $parentHelpPageId;
+
+    
+    /** A cached array of subpages and contained control items. */
+    private $subItems;
+    
+    
+    /**
+     * Constructs a HelpPage by id.
+     *
+     * @param int $id Id of the page. Default (null) will create a new help page.
+     */
+    public function __construct($id = null)
+    {
+        if ($id !== null)
+        {
+            $this->helpPageId = $id;
+            $this->load();
+        }
+    }
+    
+    
+    // Standard entity functions.
+    
+    protected function getTableName()
+    {
+        return 'HelpPages';
+    }
+    
+    protected function getPrimaryKeys()
+    {
+        return array('helpPageId');
+    }
+    
+    protected function getColumns()
+    {
+        return array('pageName', 'content', 'parentHelpPageId');
+    }
+    
+    protected function getColumnTypes()
+    {
+        return array(
+            'helpPageId'       => 'int',
+            'pageName'         => 'string',
+            'content'          => 'string',
+            'parentHelpPageId' => 'int'
+        );
+    }
+    
+    
+    // Helpers.
+    
+    /**
+     * Get and load all subpages and control items contained by this page.  
+     *
+     * @return array An array of loaded HelpPage and HelpContolItem entities. 
+     * 
+     * @throws EntityException If the parentHelpPageId of this entity is not set.
+     */
+    public function getChildren()
+    {
+        // Confirm that id is set.
+        if($this->parentHelpPageId === null)
+        {
+            throw new EntityException('entity-primary-keys-not-set');
+        }
+        
+        // If not yet set, load children.
+        if($this->subItems === null)
+        {
+            $this->subItems = array();
+            
+            // Query all subpages.
+            $pages = $this->getSelectQuery()->where('parentHelpPageId = :id')
+                                            ->execute(array('id' => $this->helpPageId));
+            
+            // Query contained HelpControlItems.
+            $citems = Query::select('controlItemName')
+                           ->from('HelpContolItems')
+                           ->where('helpPageId = :id')
+                           ->execute(array('id' => $this->helpPageId));
+            
+            
+            // Add loaded entities to result.
+            foreach($pages as $page)
+            {
+                $this->subItems[] = new HelpPage($page->getValue('helpPageId'));
+            }
+            foreach($citems as $citem)
+            {
+                $this->subItems[] = new HelpContolItem($citem->getValue('controlItemId'));
+            }
+        }
+        
+        return $this->subItems;
+    }
+    
+    
+    // Getters and setters.
+    
+    public function getHelpPageId()    { return $this->helpPageId; }
+    public function setHelpPageId($id) { $this->helpPageId = $id;  }
+    
+    public function getPageName()      { return $this->pageName;  }
+    public function setPageName($name) { $this->pageName = $name; }
+    
+    public function getContent()     { return $this->content; }
+    public function setContent($con) { $this->content = $con; }
+    
+    public function getParentHelpPageId()    { return $this->parentHelpPageId; }
+    public function setParentHelpPageId($id) { $this->parentHelpPageId = $id;  }
+}
