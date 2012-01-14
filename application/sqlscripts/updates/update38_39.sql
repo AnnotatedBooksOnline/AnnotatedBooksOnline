46880a65ded7115c17de88545ca63267523cc1ad
tomtervoort <tomtervoort@gmail.com>
2012-01-14 12:44:33 +0100

    Minor addition to last update script.
    COLLAB-356

--- application/sqlscripts/updates/update38_39.sql
+++ application/sqlscripts/updates/update38_39.sql
@@ -6,4 +6,7 @@ UPDATE "Scans" SET "status" = 5 WHERE "status" = 0;
 -- Check is no longer correct, could just as well be removed.
 ALTER TABLE "Scans" DROP CONSTRAINT "Scans_check";
 
+-- Adding mail-from-name-setting.
+INSERT INTO "Settings" ("settingName", "settingValue") VALUES ('mail-from-name', 'Collaboratory for the History of Reading');
+
 COMMIT;
