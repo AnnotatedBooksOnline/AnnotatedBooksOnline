00d5ad125ef79e80e97adc63f66e6bec3a93654c
Bert Massop <B.Massop@students.uu.nl>
2012-01-21 20:35:21 +0100

    Make export confirmation more informative.
    COLLAB-362

--- application/www/backend/controllers/authorcontroller.php
+++ application/www/backend/controllers/authorcontroller.php
@@ -3,6 +3,7 @@
 
 require_once 'controllers/controllerbase.php';
 require_once 'models/author/authorlist.php';
+require_once 'models/person/person.php';
 
 /**
  * Author controller class.
@@ -14,7 +15,30 @@ class AuthorController extends ControllerBase
      */
     public function actionLoad($data)
     {
-        // Handle load.
-        return $this->handleLoad($data, 'Author');
+        $records = array();
+        $filters = self::getArray($data, 'filters');
+        foreach ($filters as $filter)
+        {
+            if (self::getString($filter, 'column') == 'bookId')
+            {
+                $bookId = self::getInteger($filter, 'value');
+                $authors = AuthorList::find(array('bookId' => $bookId));
+                foreach ($authors as $author)
+                {
+                    $person = new Person($author->getPersonId());
+                    $records[] = array(
+                        'bookId'   => $author->getBookId(),
+                        'personId' => $author->getPersonId(),
+                        'name'     => $person->getName()
+                    );
+                }
+                break;
+            }
+        }
+        return array(
+            'records' => $records,
+            'total'   => count($records)
+        );
     }
 }
+
