fd3755f1e86f6894f563f84693cf61077ec0fc5f
tomtervoort <tomtervoort@gmail.com>
2012-01-22 15:43:04 +0100

    Added controller action to check whether auto user acceptance is turned on.
    COLLAB-393

--- application/www/backend/controllers/useractivationcontroller.php
+++ application/www/backend/controllers/useractivationcontroller.php
@@ -181,6 +181,14 @@ class UserActivationController extends Controller
         });
     }
     
+    public function actionGetAutoUserAcceptance()
+    {
+        // Check permissions.
+        Authentication::assertPermissionTo('change-global-settings');
+        
+        return Setting::getSetting('auto-user-acceptance') == '1';
+    }
+    
     /**
      * Sends another activation mail in case a user did not receive or lost the initial one.
      * 
