7327cf40ead53cb3ffd3589abdc533c335370954
jeroen <hanselmanj@hotmail.com>
2012-01-22 21:59:04 +0100

    Added bookinformationpanel COLLAB-357

--- application/www/backend/controllers/booklanguagecontroller.php
+++ application/www/backend/controllers/booklanguagecontroller.php
@@ -16,6 +16,16 @@ class BookLanguageController extends ControllerBase
     public function actionLoad($data)
     {
         // Handle load.
-        return $this->handleLoad($data, 'BookLanguage');
+        $result = $this->handleLoad($data, 'BookLanguage');
+                
+        // Also load the name of each language.
+        foreach ($result['records'] as &$record)
+        {
+            $language = new Language($record['languageId']);
+            
+            $record['languageName'] = $language->getLanguageName();
+        }
+        
+        return $result;
     }
 }
