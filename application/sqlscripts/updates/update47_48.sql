c69b139e3c55b03610b9eacca3782fdbf451031b
Bert Massop <B.Massop@students.uu.nl>
2012-01-22 01:07:19 +0100

    Fix update47_48.sql to function with existing data.

--- application/sqlscripts/updates/update47_48.sql
+++ application/sqlscripts/updates/update47_48.sql
@@ -1,10 +1,15 @@
-﻿BEGIN TRANSACTION;
+BEGIN TRANSACTION;
 
 -- Add order column to Annotations.
-ALTER TABLE "Annotations" ADD COLUMN "order" int NOT NULL;
+ALTER TABLE "Annotations" ADD COLUMN "order" int;
+UPDATE "Annotations" a SET "order" = (SELECT COUNT(*) FROM "Annotations" f WHERE f."annotationId" <= a."annotationId" AND f."scanId" = a."scanId");
+ALTER TABLE "Annotations" ALTER COLUMN "order" SET NOT NULL;
 
 -- Set transcription columns to be not NULL.
+UPDATE "Annotations" SET "transcriptionEng" = '' WHERE "transcriptionEng" IS NULL;
+UPDATE "Annotations" SET "transcriptionOrig" = '' WHERE "transcriptionOrig" IS NULL;
 ALTER TABLE "Annotations" ALTER COLUMN "transcriptionEng" SET NOT NULL;
 ALTER TABLE "Annotations" ALTER COLUMN "transcriptionOrig" SET NOT NULL;
 
 COMMIT;
+
