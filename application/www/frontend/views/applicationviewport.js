d75037b7523162312a67f8dc925537a3af8e9be1
Mathijs Baaijens <mathijsbaaijens@mathijsje-L.local>
2012-01-23 04:46:17 +0100

    Existingbinding

--- application/www/frontend/views/applicationviewport.js
+++ application/www/frontend/views/applicationviewport.js
@@ -406,7 +406,7 @@ Ext.define('Ext.ux.ApplicationViewport', {
                 
                 // Add a reorder scan tab.
                 Ext.apply(tabConfig, {
-                    title: isExistingBinding ? 'Upload (reorder scans)' : 'Modify binding (reorder scans)',
+                    title: isExistingBinding === true ? 'Upload (reorder scans)' : 'Modify binding (reorder scans)',
                     xtype: 'reorderscanform',
                     bindingId: data[0],
                     isExistingBinding: isExistingBinding
@@ -432,7 +432,7 @@ Ext.define('Ext.ux.ApplicationViewport', {
                 
                 // Add a select book tab.
                 Ext.apply(tabConfig, {
-                    title: isExistingBinding ? 'Upload (select books)' : 'Modify binding (select books)',
+                    title: isExistingBinding === true ? 'Upload (select books)' : 'Modify binding (select books)',
                     xtype: 'selectbookform',
                     bindingId: data[0],
                     isExistingBinding: isExistingBinding
