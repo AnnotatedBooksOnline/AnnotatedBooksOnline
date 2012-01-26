9d71685d306adb212cf721cee6fb820c7a8676f7
Mathijs Baaijens <mathijsbaaijens@mathijsje-L.local>
2012-01-27 00:05:22 +0100

    Script toch niet nodig.

--- application/sqlscripts/updates/update60_61.sql
+++
@@ -1,6 +0,0 @@
-START TRANSACTION;
-
-INSERT INTO "Permissions" ("actionName", "minRank") VALUES ('modify-bindings', 40);
-INSERT INTO "Permissions" ("actionName", "minRank") VALUES ('delete-bindings', 50);
-
-COMMIT;
