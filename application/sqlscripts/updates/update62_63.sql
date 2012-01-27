4b2f301d64efcba967c8e59af88ca6421de98446
Renze Droog <renzedroog@gmail.com>
2012-01-27 23:32:57 +0100

    foreign key userId -> createdUserId

--- application/sqlscripts/updates/update62_63.sql
+++ application/sqlscripts/updates/update62_63.sql
@@ -7,7 +7,7 @@ UPDATE "Annotations" SET "changedUserId" = "userId";
 ALTER TABLE "Annotations" ALTER COLUMN "changedUserId" SET NOT NULL;
 ALTER TABLE "Annotations" RENAME COLUMN "userId" TO "createdUserId";
 
-ALTER TABLE "Annotations" ADD CONSTRAINT "Annotations_changedUserId_fkey" FOREIGN KEY ("userId")
+ALTER TABLE "Annotations" ADD CONSTRAINT "Annotations_changedUserId_fkey" FOREIGN KEY ("createdUserId")
     REFERENCES "Users" ("userId") MATCH SIMPLE
     ON UPDATE NO ACTION ON DELETE NO ACTION;
 
