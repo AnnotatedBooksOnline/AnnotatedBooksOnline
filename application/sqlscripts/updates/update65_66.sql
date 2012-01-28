58b9b0173b14502135f5494b4685490337659df0
tomtervoort <tomtervoort@gmail.com>
2012-01-28 12:44:50 +0100

    Enforced uniqueness of titles of HelpParagraphs with the same parent.
    COLLAB-447

---
+++ application/sqlscripts/updates/update65_66.sql
@@ -0,0 +1,6 @@
+﻿BEGIN TRANSACTION;
+
+-- Enfore uniqueness of the combination of title and parent in helpparagraph.
+ALTER TABLE "HelpParagraphs" ADD UNIQUE ("helpPageId", "paragraphParentId", "title");
+
+COMMIT;
