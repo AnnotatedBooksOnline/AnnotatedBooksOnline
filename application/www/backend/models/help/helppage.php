5df8c9eeaa26d62f71d8608ab79a722e0e0b6f96
Bert Massop <B.Massop@students.uu.nl>
2012-02-03 00:27:38 +0100

    Replaces all occurences of //[[GPL]] with the proper license.
    COLLAB-538

--- application/www/backend/models/help/helppage.php
+++ application/www/backend/models/help/helppage.php
@@ -1,5 +1,18 @@
 <?php
-//[[GPL]]
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * Copyright: Mathijs Baaijens, Iris Bekker, Renze Droog,
+ * Maarten van Duren, Jeroen Hanselman, Bert Massop, Robin van der Ploeg,
+ * Tom Tervoort, Gerben van Veenendaal, Tom Wennink.
+ */
 
 require_once 'framework/database/entity.php';
 require_once 'models/help/helpparagraph.php';
