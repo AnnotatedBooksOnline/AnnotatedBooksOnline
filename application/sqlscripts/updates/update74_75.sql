108035f9d14478a25dfebdc4d43334ee40e28876
Tom Tervoort <tomervoort@gmail.com>
2012-08-24 23:22:28 +0200

    Fixed latest updates so they use ANI quotes.

---
+++ application/sqlscripts/updates/update74_75.sql
@@ -0,0 +1,12 @@
+SET NAMES 'utf8' COLLATE 'utf8_unicode_ci';
+SET storage_engine = INNODB;
+SET sql_mode = 'ANSI_QUOTES';
+
+USE abo;
+
+START TRANSACTION;
+
+-- Drop the UNIQUE constraint on signature and libraryId.
+DROP INDEX "signature" ON "Bindings";
+
+COMMIT;
