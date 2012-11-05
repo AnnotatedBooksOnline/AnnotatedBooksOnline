aae4726af88c91755d2384d2b2f4455866127cbc
Bert Massop <B.Massop@students.uu.nl>
2012-11-05 11:00:03 +0100

    Fix cronjob.

--- tilepyramidbuilder/cronjob/cronjob.sh
+++ tilepyramidbuilder/cronjob/cronjob.sh
@@ -3,10 +3,13 @@
 # Runs the pyramid builder every five seconds.
 # Requires the path to the backend folder as its argument.
 
-#cd /home/abo/AnnotatedBooksOnline/tilepyramidbuilder/cronjob
+MY_DIR="$0"
+
+cd "$(dirname $MY_DIR)"
 
 while true
 do
-    php tilepyramidbuilder.php "$1" 2>&1 >> "$1"/../data/logs/builderoutput.txt
+    echo -n "." >> "$1"/../data/logs/builderoutput.txt
+    php tilepyramidbuilder.php "$1" >> "$1"/../data/logs/builderoutput.txt 2>&1
     sleep 5
 done
