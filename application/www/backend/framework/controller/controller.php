9492242969a7c061c5d761b1a1594fc17f368ce6
Bert Massop <B.Massop@students.uu.nl>
2013-01-18 12:03:50 +0100

    Allow to specify different Controller output formats for easy debugging.

--- application/www/backend/framework/controller/controller.php
+++ application/www/backend/framework/controller/controller.php
@@ -54,9 +54,10 @@ abstract class Controller
         
         // Check whether we want to use the post method.
         $postMethod = ($_SERVER['REQUEST_METHOD'] == 'POST') && empty($_FILES);
+        $format = self::getString($_GET, 'format', '');
         
         // Set the appropriate content type for a JSON response.
-        if ($postMethod)
+        if ($postMethod || $format === "json")
         {
             header('Content-Type: application/json');
         }
@@ -109,13 +110,18 @@ abstract class Controller
             }
             
             // Handle output.
-            if ($postMethod)
+            if ($postMethod || $format === 'json')
             {
                 // Show output as JSON.
                 echo json_encode($output);
             }
             else if ($success)
             {
+                if ($format === "plain" || !is_string($output))
+                {
+                    header('Content-Type: text/plain');
+                }
+                
                 // Just show output.
                 if (is_string($output))
                 {
